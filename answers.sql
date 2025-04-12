d6791a334cab6ec528df7ca1fba30bc461c9806b
kdz-23 <koroso.dz@gmail.com>
2025-04-12 23:14:32 +0300

    Updates

--- answers.sql
+++ answers.sql
@@ -5,23 +5,25 @@ CREATE DATABASE bookstore_db;
 USE bookstore_db;
 
 -- Create Role
-
-CREATE ROLE developer;
+CREATE ROLE 'admin_role'
+CREATE ROLE 'developer';
 
 -- Grant Permissions to the Role
+GRANT ALL PRIVILEGES ON *.* TO 'admin_role' WITH GRANT OPTION;
 GRANT SELECT, INSERT, UPDATE, DELETE ON bookstore_db.* TO developer;
 
 -- Users of the Created Database
 
-CREATE USER 'David'@'%' IDENTIFIED BY 'D8d4-!789';
-CREATE USER 'Sumaya'@'%' IDENTIFIED BY '$uM@ya-43';
-CREATE USER 'Grace'@'%' IDENTIFIED BY 'Gr%-*ce!'
+CREATE USER 'David'@'localhost' IDENTIFIED BY 'D8d4-!789';
+CREATE USER 'Sumaya'@'localhost' IDENTIFIED BY '$uM@ya-43';
+CREATE USER 'Grace'@'localhost' IDENTIFIED BY 'Gr%-*ce!';
 
 -- Assigning Roles to each User
 
-GRANT developer TO 'David'@'%';
-GRANT developer TO 'Sumaya'@'%';
-GRANT developer TO 'Grace'@'%';
+GRANT admin_role TO 'David'@'localhost';
+GRANT developer TO 'Sumaya'@'localhost';
+GRANT developer TO 'Grace'@'localhost';
+
 
 -- Table Creation 
 
@@ -29,21 +31,35 @@ GRANT developer TO 'Grace'@'%';
 CREATE TABLE book (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     publication_year YEAR NOT NULL,
-    language_id INT REFERENCES language(language_id) ON DELETE SET NULL ON UPDATE CASCADE,
-    publisher_id INT REFERENCES publisher(publisher_id) ON DELETE SET NULL ON UPDATE CASCADE,
+    language_id INT,
+    publisher_id INT,
     price DECIMAL(10, 2) NOT NULL,
-    stock_quantity INT DEFAULT 0
-);
+    stock_quantity INT DEFAULT 0,
 
+    FOREIGN KEY (language_id) REFERENCES language(language_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 
 -- 2. Book Author Table
 CREATE TABLE book_author (
-    book_id INT REFERENCES book(book_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    author_id INT REFERENCES author(author_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    
-    PRIMARY KEY (book_id, author_id)
+    book_id INT,
+    author_id INT,
+
+    PRIMARY KEY (book_id, author_id),
+
+    FOREIGN KEY (book_id) REFERENCES book(book_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (author_id) REFERENCES author(author_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
+
 
 -- 3. Author Table
 CREATE TABLE author (
